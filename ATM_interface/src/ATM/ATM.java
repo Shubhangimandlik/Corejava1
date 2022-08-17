c4b9266c38a7be1606a000160206ce0ed562c373
Shubhangimandlik <108507739+Shubhangimandlik@users.noreply.github.com>
2022-08-16 22:01:19 -0700

    Add files via upload

---
+++ ATM_interface/src/ATM/ATM.java
@@ -0,0 +1,18 @@
+package ATM;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu{
+
+public static void main(String[] args) throws IOException {
+
+			OptionMenu op=new OptionMenu();
+			 
+			op.getLogin();
+			}
+
+		
+
+	}
+
+
